4b47364e3ee579bbeee88827331d71f23fa5ed29
Lnzarn <enriquelanzaron@gmail.com>
2026-03-17 17:57:49 +0800

    Changed the name so its not confusing, and added a getter for session

--- backend/dbsettings.py
+++ backend/dbsettings.py
@@ -1,5 +1,13 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 
-DATABASE_URL = "mysql+pymysql://root:admin123@localhost/aegis"
+# change to your mysql. We will move this in the future.
+DATABASE_URL = "mysql+pymysql://root:lanzsql@localhost/aegisdb"
 
 engine = create_engine(DATABASE_URL, echo=True)
+
+# getter method for session
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
