aa23368062c3332e280e5b196c88f3eb60ce38d4
Penkeykkk <132587602+Penkeykkk@users.noreply.github.com>
2026-03-19 22:22:43 +0800

    HEWUADUUODHWIAUDH

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,7 @@
 document.getElementById("btn_go_to_dashboard").addEventListener("click", function() {
     window.location.href = "dashboard.html";
 });
+
+document.getElementById("btn_go_to_wallet").addEventListener("click", function() {
+    window.location.href = "connectwallet.html";
+});
